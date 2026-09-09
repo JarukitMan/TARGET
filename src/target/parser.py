1109e370f55ce4bf4126255a0dea5ac1c9d4aebb
JarukitMan <pleaseletmeinijustwanttoemail@gmail.com>
2026-09-09 11:46:22 +0700

    fix: Bad dictionary key existence check pattern 	modified:   src/target/parser.py

--- src/target/parser.py
+++ src/target/parser.py
@@ -13,43 +13,47 @@ def parse_config(config: str) -> c.Configuration:
     dictionary = y.load(config, y.FullLoader)
 
     # Extracted environment
-    environment = dictionary["Environment"]
-    if environment is not None:
-        time = environment["Time"]
-        weather = environment["Weather"]
+    if "Environment" in dictionary:
+        environment = dictionary["Environment"]
 
-        if time is not None:
+        if "Time" in environment:
+            time = environment["Time"]
             configuration = c.Configuration(configuration.actors, configuration.road, configuration.weather, time)
-        if weather is not None:
+
+        if "Weather" in environment:
+            weather = environment["Weather"]
             configuration = c.Configuration(configuration.actors, configuration.road, weather, configuration.time)
 
     # It's pretty long so I put it in a different function
     actors = get_actors(dictionary["Actors"])
     configuration = c.Configuration(actors, configuration.road, configuration.weather, configuration.time)
 
-    road_network = dictionary["Road network"]
-    if road_network is not None:
-        road_marker = road_network["Road marker"]
-        # Extra
-        road_one_way = road_network["Road one way"]
-        road_lane_count = road_network["Road lane count"]
-        # End extra
-        road_type = road_network["Road type"]
-        traffic_signs = road_network["Traffic signs"]
+    if "Road network" in dictionary:
+        road_network = dictionary["Road network"]
 
-        if road_marker is not None:
+
+        if "Road marker" in road_network:
+            road_marker = road_network["Road marker"]
             road = c.Road(road.lane_count, road.one_way, road_marker, road.road_type, road.props)
-        if road_one_way is not None:
+        # Extra
+        if "Road one way" in road_network:
+            road_one_way = road_network["Road one way"]
             road = c.Road(road.lane_count, road_one_way, road.marker, road.road_type, road.props)
-        if isinstance(road_lane_count, int):
-            road = c.Road(road_lane_count, road.one_way, road.marker, road.road_type, road.props)
-        if road_type is not None:
+        if "Road lane count" in road_network:
+            road_lane_count = road_network["Road lane count"]
+            if isinstance(road_lane_count, int):
+                road = c.Road(road_lane_count, road.one_way, road.marker, road.road_type, road.props)
+        # End extra
+        if "Road type" in road_network:
+            road_type = road_network["Road type"]
             road = c.Road(road.lane_count, road.one_way, road.marker, road_type, road.props)
-        # Signs might be a list because I extended it.
-        if isinstance(traffic_signs, str):
-            road = c.Road(road.lane_count, road.one_way, road.marker, road.road_type, [traffic_signs])
-        elif isinstance(traffic_signs, list):
-            road = c.Road(road.lane_count, road.one_way, road.marker, road.road_type, traffic_signs)
+        if "Traffic signs" in road_network:
+            traffic_signs = road_network["Traffic signs"]
+            # Signs might be a list because I extended it.
+            if isinstance(traffic_signs, str):
+                road = c.Road(road.lane_count, road.one_way, road.marker, road.road_type, [traffic_signs])
+            elif isinstance(traffic_signs, list):
+                road = c.Road(road.lane_count, road.one_way, road.marker, road.road_type, traffic_signs)
         configuration = c.Configuration(configuration.actors, road, configuration.weather, configuration.time)
 
     return configuration
@@ -57,7 +61,7 @@ def parse_config(config: str) -> c.Configuration:
 def get_actors(actors_dictionary: dict) -> list[c.Actor]:
     actors: list[c.Actor] = []
     if actors_dictionary is not None:
-        for actor_name, actor_dictionary in actors_dictionary:
+        for actor_name, actor_dictionary in actors_dictionary.items():
             # Default actor
             actor_relation = c.Relation(c.RelationType.FRONT, c.Distance.MEDIUM, "ego")
             actor = c.Actor(actor_name, actor_relation, c.Behavior.GO_FORWARD)
@@ -66,23 +70,23 @@ def get_actors(actors_dictionary: dict) -> list[c.Actor]:
             if actor_name == "Ego vehicle":
                 actor = c.Actor("ego", actor.relation, actor.behavior)
 
-            actor_behavior = actor_dictionary["Behavior"]
-            if actor_behavior is not None:
+            if "Behavior" in actor_dictionary:
+                actor_behavior = actor_dictionary["Behavior"]
                 actor = c.Actor(actor.name, actor.relation, actor_behavior)
 
-            actor_position = actor_dictionary["Position"]
-            if actor_position is not None:
-                actor_position_reference = actor_position["Position reference"]
-                actor_position_relation = actor_position["Position relation"]
-                actor_distance = actor_position["Distance from reference"]
-                # Fallback
-                actor_distance_number = c.Distance.MEDIUM
+            if "Position" in actor_dictionary:
+                actor_position = actor_dictionary["Position"]
 
-                if actor_position_reference is not None:
+                if "Position reference" in actor_position:
+                    actor_position_reference = actor_position["Position reference"]
                     actor_relation = c.Relation(actor_relation.relation, actor_relation.distance, actor_position_reference)
-                if actor_position_relation is not None:
+
+                if "Position relation" in actor_position:
+                    actor_position_relation = actor_position["Position relation"]
                     actor_relation = c.Relation(actor_position_relation, actor_relation.distance, actor_relation.object_name)
-                if actor_distance is not None:
+
+                if "Distance from reference" in actor_position:
+                    actor_distance = actor_position["Distance from reference"]
                     if actor_distance == "contact":
                         actor_distance_number = c.Distance.CONTACT
                     elif actor_distance == "immediate":
@@ -93,8 +97,11 @@ def get_actors(actors_dictionary: dict) -> list[c.Actor]:
                         actor_distance_number = c.Distance.MEDIUM
                     elif actor_distance == "far":
                         actor_distance_number = c.Distance.FAR
+                    else:
+                        # Fallback
+                        actor_distance_number = c.Distance.MEDIUM
+                    actor_relation = c.Relation(actor_relation.relation, actor_distance_number, actor_relation.object_name)
 
-                actor_relation = c.Relation(actor_relation.relation, actor_distance_number, actor_relation.object_name)
                 actor = c.Actor(actor.name, actor_relation, actor.behavior)
 
             actors.append(actor)
